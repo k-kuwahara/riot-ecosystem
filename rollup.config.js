4c03a129cdc6e8b98ec4646e31fa12dbd5e0b9ce
k-kuwahara <zensin0082@gmail.com>
2018-02-19 19:52:05 +0900

    added: WIP modules about rollup plugins

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs'
+import buble    from 'rollup-plugin-buble'
+import progress from 'rollup0plugin-progress'
+import riot     from 'rollup-plugin-riot'
+
+const namedExports = {};
+
+export default {
+  input: 'src/app.js',
+  output: {
+    file: 'dist/bundle.js',
+    sourcemap: 'false',
+    exports: 'none',
+    format: 'iife',
+    strict: true
+  },
+  context: 'window',
+  plugins: [
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: namedExports
+    }),
+    buble(),
+    progress()
+  ]
+}
