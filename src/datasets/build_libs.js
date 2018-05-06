af413feec319ed5caca96a2ae4d550a4a87f6740
k-kuwahara <zensin0082@gmail.com>
2018-05-07 08:25:06 +0900

    WIP

---
+++ src/datasets/build_libs.js
@@ -0,0 +1,7 @@
+export default [
+   {name: 'riot-tag-loader', url: 'riot/tag-loader'},
+   {name: 'tag-loader', url: 'srackham/tag-loader'},
+   {name: 'rollup-plugin-riot', url: 'riot/rollup-plugin-riot'},
+   {name: 'riotify', url: 'riot/riotify'},
+   {name: 'gulp-riot', url: 'e-jigsaw/gulp-riot'}
+]
---
+++ src/datasets/test_libs.js
@@ -0,0 +1,3 @@
+export default [
+   {name: 'karma-riot', url: 'riot/karma-riot'}
+]
